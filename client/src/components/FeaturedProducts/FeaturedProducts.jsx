17f0a3f653b0c1e34e26b0de88480753ce14aa9b
emlegweak <emilythearmstrong@gmail.com>
2022-12-22 22:21:29 -0500

    created slider, featuredProducts

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './FeaturedProducts.scss'
+import Card from '../Card/Card'
+
+const FeaturedProducts = ({type}) => {
+    let data = []
+  return (
+    <div className="featuredProducts">
+        <div className="top">
+            <h1>{type} Products</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium voluptate asperiores, aliquam repellat in dolorum dicta voluptatum commodi a laboriosam vero architecto, odio eaque facilis. Aliquam dolorum magni non totam.</p>
+        </div>
+        <div className="bottom">
+            {data?.map(item => {
+                <Card item={item} key={item.id} />
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedProducts
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import FeaturedProducts from '../FeaturedProducts/FeaturedProducts'
 import Slider from '../Slider/Slider'
 
 import './Home.scss'
@@ -7,6 +8,8 @@ const Home = () => {
   return (
    <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
    </div>
   )
 }
